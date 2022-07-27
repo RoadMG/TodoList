51afb21f9c0b3347720ba690728853693960f12b
Mingyu Kil <90road@gmail.com>
2022-07-27 10:02:01 +0900

    first

---
+++ src/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RecoilRoot } from "recoil";
+import { darkTheme } from "./theme";
+import App from "./App";
+import { ThemeProvider } from "styled-components";
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
+  <RecoilRoot>
+    <ThemeProvider theme={darkTheme}>
+      <App />
+    </ThemeProvider>
+  </RecoilRoot>
+);
